391c2edce96f04d0d79d9ec3b1dad2e27e09bc21
Shaddime <shadrackmwangi6@gmail.com>
2024-04-11 22:27:03 -0400

    modified Index.html, Style.Css and Script.js

--- script.js
+++ script.js
@@ -2,18 +2,22 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the count
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
 
-let countEl = document.getElementById("count-el")
+console.log(saveEl);
 
-
-let count = 0
+let count = 0;
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count 
+    count += 1;
+    countEl.innerText = count; 
     console.log(count);
 }
 
 function save() {
-    console.log(count);
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
 }
